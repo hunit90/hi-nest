c72a2c57ae93b0ad5a51e19e39d128194bbab5f9
hunit <hunit612@naver.com>
2023-08-01 23:58:53 +0900

    feat: add moviecontroller

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'This will return all movices';
+  }
+
+  @Get('/:id')
+  getOne(){
+    return 'This will one movie';
+  }
+}
