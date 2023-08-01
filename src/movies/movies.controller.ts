daa91dca054790500750353e6341d266364fc23d
hunit <hunit612@naver.com>
2023-08-02 00:19:41 +0900

    feat: add crud

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,8 +16,32 @@ export class MoviesController {
     return 'This will return all movices';
   }
 
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie with a title: ${searchingYear}`;
+  }
+
   @Get('/:id')
-  getOne(){
-    return 'This will one movie';
+  getOne(@Param('id') id: string) {
+    return `This will return one movie with the id: ${id}`;
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    console.log(movieData);
+    return 'This will create a movie';
+  }
+
+  @Delete(':/id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie with the id ${movieId}`;
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updateMovie: movieId,
+      ...updateData,
+    };
   }
 }
