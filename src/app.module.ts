6a262eda88f4946609e54419fc238ce2bef39c93
hunit <hunit612@naver.com>
2024-03-26 23:54:27 +0900

    post 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeormConfig } from './config/typeorm.config';
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { TypeormConfig } from './config/typeorm.config';
       useClass: TypeormConfig,
     }),
     UserModule,
+    PostModule,
   ],
   controllers: [AppController],
   providers: [],
